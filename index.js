45849ed2ae4c5deea8b10f886dac268017f38bb5
PigeonPosse <pigeonpossemail@gmail.com>
2022-08-18 17:04:46 +0200

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
 #!/usr/bin/env node
 
+/**
+ * INDEX FILE
+ * 
+ * File used to create the cli
+ * 
+ * since version 1.1.2
+ */
+
 import * as path         from 'path';
 import { fileURLToPath } from 'url';
 import { execFile }      from 'child_process';
+import * as fs           from 'fs';
 
-const __dirname = path.dirname( fileURLToPath( import.meta.url ) );
-const json      = path.resolve( __dirname, '.htmlhintrc.json' );
-const htmlhint  = path.resolve( __dirname, 'node_modules/.bin/htmlhint' );
+/**
+ * Get paths and files
+ */
+const __dirname   = path.dirname( fileURLToPath( import.meta.url ) );
+const json        = path.resolve( __dirname, '.htmlhintrc.json' );
+const htmlhintBin = ( () => {
 
+	let file;
+
+	file = path.resolve( __dirname, 'node_modules/.bin/htmlhint' );
+  
+	if ( fs.existsSync( file ) ) {
+
+		return file;
+	
+	}
+
+	file = './node_modules/.bin/htmlhint';
+
+	if ( fs.existsSync( file ) ) {
+
+		return file;
+	
+	}
+
+	console.error( 'You have to install Htmlhint package' );
+	console.log( 'You can install the package with: npm install htmlhint' );
+
+	return false;
+
+} )();
+
+/**
+ * Set exec
+ */
 const execFunct = ( name, args = {} ) => {
 
 	execFile( 
 		name,
 		args,
 		( error, stdout, stderr ) => {
-
-			// if ( error ) {
-
-			// 	console.error(  `error: ${error.message}` );
-			
-			// }
 
 			if ( stdout ) {
 
@@ -38,5 +72,12 @@ const execFunct = ( name, args = {} ) => {
 
 };
 
-execFunct( htmlhint, [ '-c', json ] );
+/**
+ * Run exec
+ */
+if ( htmlhintBin ) {
+	
+	execFunct( htmlhintBin, [ '-c', json ] );
+
+}
 
